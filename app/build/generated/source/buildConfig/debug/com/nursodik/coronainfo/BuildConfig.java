61f5357f77e06cc884f48230d916014115db9504
nursodik <62178561+ndik@users.noreply.github.com>
2020-07-13 10:14:51 +0700

    UAS

--- app/build/generated/source/buildConfig/debug/com/nursodik/coronainfo/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/nursodik/coronainfo/BuildConfig.java
@@ -5,7 +5,7 @@ package com.nursodik.coronainfo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.azhar.coronainfo";
+  public static final String APPLICATION_ID = "com.nursodik.coronainfo";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
